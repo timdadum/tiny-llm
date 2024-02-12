d29a7c426e79998e92bac560e999d92a3928198f
timdadum <74312568+timdadum@users.noreply.github.com>
2024-02-12 14:25:15 +0100

    New bugfix: device of generated_ids in Baseline_rnn.sample

--- nn_models/baseline_rnn.py
+++ nn_models/baseline_rnn.py
@@ -41,7 +41,8 @@ class BaselineModel(nn.Module):
             input_ids = torch.cat((padding, input_ids), dim=1)
 
         # Initialie the tensor for generated IDs
-        generated_ids = torch.tensor(input_ids, dtype=torch.float32)
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        generated_ids = torch.tensor(input_ids, dtype=torch.float32).to(device)
 
         for _ in range(generation_length):
             # Get output logits
